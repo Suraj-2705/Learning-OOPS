1bd4f90bf9b4060631732e9acd8313169889f936
Suraj <surajyadav052005@gmail.com>
2024-09-23 13:41:51 +0530

    file handling

--- class.cpp
+++ class.cpp
@@ -1,7 +1,37 @@
 #include<iostream>
 using namespace std;
+#include<string>
+class Student{
+    int sid;
+    string name;
+    float subject1, subject2, subject3;
 
+public:
+    Student(int sid, string name, float sub1, float sub2, float sub3)
+    {
+    this->sid = sid;
+    this->name = name;
+    this->subject1 = sub1;
+    this->subject2 = sub2;
+    this->subject3 = sub3;
+    }
+    
+    void display(){
+        cout<<"Student ID: "<<sid<<endl;
+        cout<<"Student Name: "<<name<<endl;
+        cout<<"Student avg marks: "<<(subject1+subject2+subject3)/3<<endl;
+    }
 
-int main(){
+    float averageMarks(){
+        return (subject1 + subject2 + subject3)/3;
+
+    }
+};
+
+int main()
+{
+    Student s(1, "John", 90, 80, 70);
+    s.display();
+    return 0;
 
 }
