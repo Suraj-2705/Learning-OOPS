6fcd97bda5e877e0f6147b83ad35795be752333b
Suraj <surajyadav052005@gmail.com>
2024-10-24 12:38:24 +0530

    Abstraction

---
+++ abstraction.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+    public:
+        virtual void sound() = 0;
+        virtual void move() = 0;
+};
+
+
+class Bird : public Animal{
+    public:
+        void sound(){
+            cout<<"Bird Chirps"<<endl;
+            cout << endl;
+        }
+        
+        void move(){
+            cout << "Bird flies ...."<<endl;
+            cout << endl;
+        }
+        
+};
+
+class Dog : public Animal{
+    public:
+        void sound(){
+            cout<<"Bark bark" << endl;
+            cout << endl;
+        }
+
+        void move(){
+            cout << "Dog runs"<<endl;
+            cout << endl;
+        }
+};
+
+int main(){
+   Animal *a1 = new Dog();
+   Animal *a2 = new Bird();
+   a1->sound();
+   a1->move();
+   a2->sound();
+   a2->move();
+}
